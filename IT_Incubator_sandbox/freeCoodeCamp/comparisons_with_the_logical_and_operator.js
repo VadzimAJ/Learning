ae871c427858e35fae4c8f0bf29c94830b39ee24
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-08 18:08:16 +0300

    feat: complete 'Comparisons with the Logical And Operator' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/comparisons_with_the_logical_and_operator.js
@@ -0,0 +1,15 @@
+//Comparisons with the Logical And Operator
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparisons-with-the-logical-and-operator
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);

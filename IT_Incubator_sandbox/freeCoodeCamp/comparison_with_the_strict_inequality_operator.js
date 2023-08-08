f7d8507f40fc808aa93f960dcabd1c084f873339
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-08 17:00:10 +0300

    feat: complete 'Comparison with the Strict Inequality Operator' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/comparison_with_the_strict_inequality_operator.js
@@ -0,0 +1,10 @@
+// Comparison with the Strict Inequality Operator https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-strict-inequality-operator
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);

26d87e41612e2207b5d64049d1725c761a31b7af
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-09 00:15:09 +0300

    feat: complete 'Returning Boolean Values from Functions' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/returning_boolean_values_from_functions.js
@@ -0,0 +1,10 @@
+//Returning Boolean Values from Functions
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/returning-boolean-values-from-functions
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);

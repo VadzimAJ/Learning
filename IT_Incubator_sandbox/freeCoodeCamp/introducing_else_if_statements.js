68481ad673085b7e7d6a941f82fde813c06f31d8
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-08 21:40:40 +0300

    feat: complete 'Introducing Else If Statements' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/introducing_else_if_statements.js
@@ -0,0 +1,18 @@
+//Introducing Else If Statements
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/introducing-else-if-statements
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else{
+    return "Between 5 and 10";
+  }
+  
+}
+
+testElseIf(7);

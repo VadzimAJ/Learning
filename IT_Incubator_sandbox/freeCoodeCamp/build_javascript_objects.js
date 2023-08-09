08e9c96f9aa10b1e40cb547c0a5f053390e7a1d5
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-09 18:14:09 +0300

    feat: complite 'Build JavaScript Objects' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/build_javascript_objects.js
@@ -0,0 +1,12 @@
+//Build JavaScript Objects
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/build-javascript-objects
+
+const myDog = {
+  // Only change code below this line
+  "name": "Chop",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Cat's", "Dogs"]
+
+  // Only change code above this line
+};

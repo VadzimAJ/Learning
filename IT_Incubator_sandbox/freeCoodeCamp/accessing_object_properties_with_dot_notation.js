c7f0236abe135c623ca125f9c10b6ed233bc2fb8
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-09 21:17:08 +0300

    feat: complite 'Accessing Object Properties with Dot Notation' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/accessing_object_properties_with_dot_notation.js
@@ -0,0 +1,13 @@
+//Accessing Object Properties with Dot Notation
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-object-properties-with-dot-notation
+
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line

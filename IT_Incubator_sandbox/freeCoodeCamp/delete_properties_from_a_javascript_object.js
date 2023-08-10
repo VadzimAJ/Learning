f3ac31ca049fb94373100c6d1b397937492e23a9
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-10 15:58:25 +0300

    feat: complite 'Delete Properties from a JavaScript Object' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/delete_properties_from_a_javascript_object.js
@@ -0,0 +1,15 @@
+//Delete Properties from a JavaScript Object
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/delete-properties-from-a-javascript-object
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+delete myDog.tails;
+
+// Only change code below this line

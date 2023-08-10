615c26e99d641ce2cb96d50c0cadaa68c8099ad5
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-10 15:54:58 +0300

    feat: complite 'Add New Properties to a JavaScript Object' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/add_new_properties_to_a_javascript_object.js
@@ -0,0 +1,11 @@
+//Add New Properties to a JavaScript Object
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/add-new-properties-to-a-javascript-object
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";

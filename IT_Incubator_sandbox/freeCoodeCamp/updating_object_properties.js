bc2cd739782e8872e020e1dd5818aafd597b563d
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-08-10 15:46:18 +0300

    feat: complite 'Updating Object Properties' lesson

---
+++ IT_Incubator_sandbox/freeCoodeCamp/updating_object_properties.js
@@ -0,0 +1,12 @@
+//Updating Object Properties
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/updating-object-properties
+
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = "Happy Coder"
+// Only change code below this line
